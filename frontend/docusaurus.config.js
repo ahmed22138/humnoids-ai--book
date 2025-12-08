c92ffdf92440af7de598674b397229f447d37235
Ahmed-malik <159722798+ahmed22138@users.noreply.github.com>
2025-12-08 21:53:23 +0500

    Change baseUrl for Docusaurus configuration

--- frontend/docusaurus.config.js
+++ frontend/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://humanoids-ai-book.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ai-textbook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
